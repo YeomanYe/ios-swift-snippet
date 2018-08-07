3082ee957416a0de5f3b2088fd61cc815025f4b1
YeomanYe <ming13821007683@gmail.com>
2018-08-07 17:10:03 +0800

    add baseTabCtrl

---
+++ io-swift-snippet/Base/BaseTabCtrl.swift
@@ -0,0 +1,37 @@
+//
+//  BaseTabCtrl.swift
+//  io-swift-snippet
+//
+//  Created by 美德远健 on 2018/8/7.
+//  Copyright © 2018年 __FWHeart__. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BaseTabCtrl : UITabBarController{
+    var tintColor:UIColor?
+    var dataSource:Array<NSDictionary>?
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder);
+        if(nil != self){
+            didInitialize();
+        }
+        return nil;
+    }
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil);
+        if(nil != self) {
+            didInitialize();
+        }
+    }
+    func didInitialize(){
+        if(nil == tintColor){
+            tintColor = UIColor.blue;
+        }
+        self.tabBar.tintColor = self.tintColor;
+    }
+    func initDataSource(){
+        
+    }
+}
---
+++ io-swift-snippet/Layout/ListViewCtrl.swift
@@ -0,0 +1,9 @@
+//
+//  ListViewCtrl.swift
+//  io-swift-snippet
+//
+//  Created by 美德远健 on 2018/8/7.
+//  Copyright © 2018年 __FWHeart__. All rights reserved.
+//
+
+import Foundation
